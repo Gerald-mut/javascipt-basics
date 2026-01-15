2d1eb8adc92d7546217c817185849fc904184146
Gerald Muteru <muterugerald@gmail.com>
2026-01-15 18:32:44 +0300

    Learnt about splice and slice

--- array.js
+++ array.js
@@ -10,3 +10,35 @@ fruits.push("Oranges"); //Oranges will be added to the end of the array
 
 fruits.shift();// Removes the first element apples
 fruits.unshift("apples") //adds apple to the beginning of the array
+
+//splice method in arrays
+//It returns an array of remved elements
+let subjects = ["Maths", "English", "Science"];
+result = subjects.splice(1,1); //["English"]
+alert(subjects); // ["Maths, Science"]
+
+let arr = ["I", "study", "JavaScript", "right", "now"];
+
+// remove 3 first elements and replace them with another
+arr.splice(0,3,"Let's", "dance");
+alert(arr); // ["Let's", "dance", "right", "now"]
+
+// Splice can also insert elements without removing one
+// You just set deleteCount to 0
+let arrr = ["I", "study", "Javascript"];
+arr.splice(2,0,"complex", "language");
+alert(arrr); // "I", "study", "complex", "language", "javascript";
+
+let digits = [1, 2, 5];
+//from index -1(one step from the end)
+//delete 0 elements
+// then insert 3 and 4
+digits.splice(-1,0,3,4);
+alert(digits); // [1, 2, 3, 4, 5]
+
+//slice method
+//arr.slice([start], [end])
+//it returns a new array copying it from start to end not including end. End is exclusive
+let name = ["T", "e", "s", "h"];
+alert(namee.slice(1,3)); //e,s
+alert(namee.slice(-2)); //s h
