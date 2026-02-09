1dc7c182222da1acd19b8f8384439aa5e1fc07a7
Gerald Muteru <muterugerald@gmail.com>
2026-02-09 12:17:22 +0300

    LEarnt about dom manipulation

--- cv/script.js
+++ cv/script.js
@@ -2,12 +2,29 @@ const firstName = document.getElementById("fname");
 const lastName = document.getElementById("lname");
 const submitButton = document.getElementById("submit-btn");
 const form = document.querySelector('form');
+const results = document.getElementById('results');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    const cvInput = e.target.elements.cv;
+    const errorMsg = document.getElementById('error');
+    
+    if (cvInput.value.trim() === "") {
+        cvInput.classList.add("invalid");
+        errorMsg.classList.remove("hidden");
+        return;
+    }
+
+    cvInput.classList.remove("invalid");
+    errorMsg.classList.add("hidden");
+
     submitButton.textContent = "Processing...";
     setTimeout(() => {
         submitButton.textContent = "Done!"
     }, 1000)
     console.log(firstName.value, lastName.value);
+})
+
+submitButton.addEventListener('click', () => {
+    results.classList.toggle("hidden");
 })
